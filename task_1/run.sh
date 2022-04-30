2af48486ff1daaa7b069131a12880ccc4b71f56f
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2022-04-30 12:39:12 +0300

    Update run.sh

--- task_1/run.sh
+++ task_1/run.sh
@@ -5,14 +5,10 @@ hdfs dfs -rm -r -skipTrash ${OUT_DIR} > /dev/null
 
 (yarn jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
         -D mapreduce.job.reduces=${NUM_REDUCERS} \
-        -D mapreduce.job.output.key.comparator.class=org.apache.hadoop.mapreduce.lib.partition.KeyFieldBasedComparator \
-        -D mapreduce.map.output.key.field.separator=. \
-        -D mapreduce.partition.keycomparator.options="-k2rn" \
-        -D stream.num.map.output.key.fields=1 \
         -files mapper.py,reducer.py \
         -mapper mapper.py \
         -reducer reducer.py \
         -input /data/wiki/en_articles_part \
         -output ${OUT_DIR} > /dev/null)
 
-hdfs dfs -cat ${OUT_DIR}/part-00000 | head -10
+hdfs dfs -cat ${OUT_DIR}/part-00000 | sort -k2nr | head -10
