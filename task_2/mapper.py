90755faa6f6a0d38a02117a335788b18da252e2a
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2022-04-30 22:33:10 +0300

    Update mapper.py

--- task_2/mapper.py
+++ task_2/mapper.py
@@ -7,19 +7,13 @@ import re
 sys.stdin = open(sys.stdin.fileno(), encoding='utf-8')
 
 for line in sys.stdin:
-    try:
-        log_line = str(line.strip()).split()
-    except ValueError as e:
-        continue
-    if len(log_line) > 7:
-        if log_line[4] == "issued" \
-                and log_line[5] == "server" \
-                and log_line[6] == "command:":
-            date = log_line[0]
-            nick = log_line[3]
-            cmd_name = log_line[7].split()[0]
-            date = date.strip("[]").split(".")[0]
-
-            print("{}\t{}\t{}".format(date, cmd_name, 1))
-
-# -D stream.num.map.output.key.fields=2 \
+    line = line.strip().split()
+    if len(line) > 7:
+        if line[4] == "issued" and line[5] == "server" and line[6] == "command:":
+            user = line[3]
+            print("{}\t{}\t{}".format(user, "command", 1))
+        
+        if len(line) > 8:
+            if line[3] == "UUID" and line[4] == "of" and line[5] == "player":
+                user = line[6]
+                print("{}\t{}\t{}".format(user, "session", 1))
