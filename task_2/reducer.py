0c36d260e018db548ee80da0cb690cd8114bb2b7
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2022-05-01 00:15:09 +0300

    Update reducer.py

--- task_2/reducer.py
+++ task_2/reducer.py
@@ -2,7 +2,7 @@
 
 import sys
 
-# sys.stdin = open(sys.stdin.fileno(), encoding='utf-8')
+sys.stdin = open(sys.stdin.fileno(), encoding='utf-8')
 
 commands = 0
 sessions = 0
@@ -11,15 +11,15 @@ for line in sys.stdin:
     if not line.strip():
         continue
     user, action, value = line.strip().split('\t', 2)
-    print("{}\t{}\t{}".format(user, 1, action))
+#     print("{}\t{}\t{}".format(user, 1, action))
     value = int(value)
     
     if cur_user is None:
         cur_user = user
 
     if cur_user != user:
-#         if sessions >= 0:
-#             print("{}\t{}\t{}".format(cur_user, int(round(commands / max(sessions, 1), 1)), sessions))
+        if sessions > 0:
+            print("{}\t{}\t{}".format(cur_user, round(commands / sessions, 1)), sessions))
         sessions = 0
         commands = 0
         cur_user = user
@@ -28,5 +28,6 @@ for line in sys.stdin:
     else:
         sessions += value
 
-# if sessions >= 0:
+if sessions >= 0:
+    print("{}\t{}\t{}".format(cur_user, round(commands / sessions, 1)), sessions))
 #     print("{}\t{}\t{}".format(cur_user, int(round(commands / max(sessions, 1), 1)), sessions))
