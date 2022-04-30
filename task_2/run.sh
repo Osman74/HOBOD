01cf839accbe98613ce31c48d0472f2483601af4
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2022-04-30 23:54:38 +0300

    Update run.sh

--- task_2/run.sh
+++ task_2/run.sh
@@ -1,11 +1,11 @@
 ##! /usr/bin/env bash
-OUT_DIR="results__111"
+OUT_DIR="results__123"
 NUM_REDUCERS=4
 hdfs dfs -rm -r -skipTrash ${OUT_DIR} > /dev/null
 
 (yarn jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
         -D mapreduce.job.reduces=${NUM_REDUCERS} \
-        -files mapper.py,reducer.py \
+        -files mapper.py, reducer.py \
         -mapper mapper.py \
         -reducer reducer.py \
         -input /data/minecraft-server-logs \
