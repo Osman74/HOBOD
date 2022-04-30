88783c3e28d09a20059bcc7c3efc815ccb6bbbe5
DanMitroshin <36602715+DanMitroshin@users.noreply.github.com>
2022-05-01 00:26:51 +0300

    Update run.sh

--- task_2/run.sh
+++ task_2/run.sh
@@ -1,7 +1,8 @@
 ##! /usr/bin/env bash
 OUT_DIR="results__123"
-NUM_REDUCERS=1
+NUM_REDUCERS=4
 hdfs dfs -rm -r -skipTrash ${OUT_DIR} > /dev/null
+hdfs dfs -rm -r -skipTrash ${OUT_DIR}_done > /dev/null
 
 (yarn jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
         -D mapreduce.job.reduces=${NUM_REDUCERS} \
@@ -9,6 +10,13 @@ hdfs dfs -rm -r -skipTrash ${OUT_DIR} > /dev/null
         -mapper mapper.py \
         -reducer reducer.py \
         -input /data/minecraft-server-logs \
+        -output ${OUT_DIR}_done > /dev/null &&
+
+yarn jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
+        -D mapreduce.job.reduces=1 \
+        -mapper cat \
+        -reducer cat \
+        -input ${OUT_DIR}_done \
         -output ${OUT_DIR} > /dev/null)
 
 hdfs dfs -cat ${OUT_DIR}/part-00000 | sort -k2nr | head -10
